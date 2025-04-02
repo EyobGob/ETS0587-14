d74e9cfa4bcd509caf7de668691f4379b9918114
Eyob Gobezie <eyob6963@gmail.com>
2025-04-02 11:32:01 +0300

    string_methods day9

---
+++ Python/String_Methods/str_islower.py
@@ -0,0 +1,17 @@
+string1 = "hello".islower() 
+print(string1)  # Returns True
+
+string2 = "Hello".islower()  
+print(string2) # Returns False
+
+string3 = "hello world".islower() 
+print(string3) # Returns True
+
+string4 = "hello World".islower() 
+print(string4) # Returns False
+
+string5 = "123".islower()  
+print(string5) # Returns False
+
+string6 = "hello123world".islower() 
+print(string6) # Returns True
---
+++ Python/String_Methods/str_isupper.py
@@ -0,0 +1,17 @@
+string1 = "HELLO".isupper()  
+print(string1) # Returns True
+
+string2 = "Hello".isupper()  
+print(string2) # Returns False
+
+string3 = "HELLO WORLD".isupper() 
+print(string3) # Returns True
+
+string4 = "HELLO world".isupper() 
+print(string4) # Returns False
+
+string5 = "123".isupper()   
+print(string5) # Returns False
+ 
+string6 = "HELLO123WORLD".isupper() 
+print(string6) # Returns True
