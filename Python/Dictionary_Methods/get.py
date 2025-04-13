9ab794938fa9a6e36c7ef994b36937a76b4eb0b1
Eyob Gobezie <eyob6963@gmail.com>
2025-04-13 22:28:43 +0300

    Dictionary_methods_part2

--- Python/Dictionary_Methods/get.py
+++ Python/Dictionary_Methods/get.py
@@ -2,10 +2,10 @@ my_dict = {"name": "Eyob", "age": 22, "city": "Addis Ababa"}
 
 # Key exists
 name = my_dict.get("name")
-print(f"Name: {name}")  # Output: Name: Alice
+print(f"Name: {name}")  # Output: Name: Eyob
 
 age = my_dict.get("age")
-print(f"Age: {age}")    # Output: Age: 30
+print(f"Age: {age}")    # Output: Age: 22
 
 # Key does not exist, no default provided (returns None)
 country = my_dict.get("country")
